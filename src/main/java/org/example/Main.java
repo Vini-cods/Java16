98cd766d49897802ba8ecbb6c8a81f82cc96144a
Vini-cods <vinisanto141@gmail.com>
2025-07-26 12:07:32 -0300

    Refactor Main class to demonstrate array initialization and iteration Add encodings.xml and vcs.xml configuration files for project setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Declaração e inicialização do array
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        // Percorrendo o array com um loop for
+        System.out.println("Percorrendo o array com for:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Elemento " + i + ": " + numeros[i]);
         }
+
+        // Percorrendo o array com um loop for-each
+        System.out.println("\nPercorrendo o array com for-each:");
+        for (int numero : numeros) {
+            System.out.println("Elemento: " + numero);
+        }
+
     }
 }
